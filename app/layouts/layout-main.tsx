4204cbd382d5326ea815a7eb46980fd62c1e11e1
boust <saulobrustolin@icloud.com>
2026-07-05 15:26:54 -0300

    adding animation to hidden floating menu to allow view back content

--- app/layouts/layout-main.tsx
+++ app/layouts/layout-main.tsx
@@ -4,6 +4,7 @@ import type { Route } from "./+types/layout-main";
 import * as LucideIcons from "lucide-react";
 import { Card } from "~/components/ui/card";
 import { menus } from "~/lib/singleton";
+import { useEffect, useState } from "react";
 
 export async function loader({ request }: Route.LoaderArgs) {
     const user = await requireAuth(request);
@@ -17,12 +18,29 @@ export async function loader({ request }: Route.LoaderArgs) {
 const LayoutMain = () => {
     const navigate = useNavigate();
 
+    const [isVisible, setIsVisible] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const controlNavbar = () => {
+            if (window.scrollY > lastScrollY && window.scrollY > 50) {
+                setIsVisible(false);
+            } else {
+                setIsVisible(true);
+            }
+            setLastScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', controlNavbar);
+        return () => window.removeEventListener('scroll', controlNavbar);
+    }, [lastScrollY]);
+
     return (
         <div className="bg-cream min-screen py-4 flex flex-col items-center">
             <main className="xl:max-w-3/4 px-4 w-full">
                 <Card className="flex flex-col gap-4 bg-transparent ring-0 p-0.5">
                     <Outlet />
-                    <div className="fixed bottom-0 sm:bottom-1 left-1/2 -translate-1/2 flex gap-8 bg-green-high p-4 px-8 rounded-full">
+                    <div className={`fixed bottom-2 sm:bottom-1 left-1/2 -translate-x-1/2 flex gap-8 bg-green-high p-4 px-8 rounded-full transition-transform duration-300 ${isVisible ? 'translate-y-0' : 'translate-y-24'}`}>
                         {menus.map(m => {
                             const IconComponent = (LucideIcons as any)[m.icon];
 
